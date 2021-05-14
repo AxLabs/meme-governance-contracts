ff54b92653bc2355ca88709a267af887f7c5642d
Michael Bucher <michael@axlabs.com>
2021-05-14 10:14:24 +0200

    Add Meme class

---
+++ src/main/java/io/neow3j/Meme.java
@@ -0,0 +1,13 @@
+package io.neow3j;
+
+public class Meme {
+    public String description;
+    public String url;
+    public String imageHash;
+    
+    public Meme(String description, String url, String imageHash) {
+        this.description = description;
+        this.url = url;
+        this.imageHash = imageHash;
+    }
+}
